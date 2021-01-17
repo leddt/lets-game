18b628419453418466244debc8fdd321499f80b6
David Thibault <david.thibault@sigmund.ca>
2021-01-17 00:27:40 -0500

    Support QR code generation for two factor auth

---
+++ LetsGame.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using LetsGame.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LetsGame.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LetsGame.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
