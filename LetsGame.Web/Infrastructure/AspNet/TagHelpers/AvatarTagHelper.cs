d11e998cb70678e016a74f5a20a2010ebafe0849
David Thibault <david.thibault@sigmund.ca>
2021-01-17 14:57:20 -0500

    Refactor groups partial

--- LetsGame.Web/Infrastructure/AspNet/TagHelpers/AvatarTagHelper.cs
+++ LetsGame.Web/Infrastructure/AspNet/TagHelpers/AvatarTagHelper.cs
@@ -1,7 +1,11 @@
-﻿using System.Text.Encodings.Web;
+﻿using System.Collections.Generic;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using LetsGame.Web.Extensions;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -10,18 +14,53 @@ namespace LetsGame.Web.Infrastructure.AspNet.TagHelpers
     [HtmlTargetElement("avatar")]
     public class AvatarTagHelper : TagHelper
     {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+        private readonly IActionContextAccessor _actionContextAccessor;
+        private IDictionary<string, string> _routeValues;
+
+        public AvatarTagHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor)
+        {
+            _urlHelperFactory = urlHelperFactory;
+            _actionContextAccessor = actionContextAccessor;
+        }
+
         public string Name { get; set; }
         public bool IsButton { get; set; }
+        public bool IsLink => !string.IsNullOrWhiteSpace(Page);
         public bool IsAdd { get; set; }
         public bool IsRemove { get; set; }
+        public bool IsLarge { get; set; }
+        
+        [HtmlAttributeName("asp-page")] public string Page { get; set; }
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public IDictionary<string, string> RouteValues
+        {
+            get => _routeValues ??= new Dictionary<string, string>();
+            set => _routeValues = value;
+        }
 
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagMode = TagMode.StartTagAndEndTag;
             output.AddClass("avatar", HtmlEncoder.Default);
             output.Attributes.SetAttribute("title", Name);
-            
-            if (IsButton)
+
+            if (IsLarge)
+            {
+                output.AddClass("avatar-large", HtmlEncoder.Default);
+            }
+
+            if (IsLink)
+            {
+                output.TagName = "a";
+                
+                var actionContext = _actionContextAccessor.ActionContext;
+                var urlHelper = _urlHelperFactory.GetUrlHelper(actionContext);
+                var url = urlHelper.Page(Page, RouteValues);
+
+                output.Attributes.SetAttribute("href", url);
+            }
+            else if (IsButton)
             {
                 output.TagName = "button";
             }
