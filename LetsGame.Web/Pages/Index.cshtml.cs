1fe644fbbeb6038abe2274f9cfad7ad8ebca35d1
David Thibault <david.thibault@sigmund.ca>
2021-01-17 19:48:02 -0500

    Home page

--- LetsGame.Web/Pages/Index.cshtml.cs
+++ LetsGame.Web/Pages/Index.cshtml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LetsGame.Web.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace LetsGame.Web.Pages
@@ -12,15 +15,21 @@ namespace LetsGame.Web.Pages
     [Authorize]
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<AppUser> _userManager;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Group> Groups { get; set; }
+        
+        public IndexModel(ApplicationDbContext db, UserManager<AppUser> userManager)
         {
-            _logger = logger;
+            _db = db;
+            _userManager = userManager;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var userId = _userManager.GetUserId(User);
+            Groups = await _db.Groups.Where(x => x.Memberships.Any(m => m.UserId == userId)).ToListAsync();
         }
     }
 }
