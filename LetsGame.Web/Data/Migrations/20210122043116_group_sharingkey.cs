4571e005e885a02dc1cc17285e72a064a6ec2fc2
David Thibault <david.thibault@sigmund.ca>
2021-01-21 23:31:49 -0500

    Add migration

---
+++ LetsGame.Web/Data/Migrations/20210122043116_group_sharingkey.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetsGame.Web.Data.Migrations
+{
+    public partial class group_sharingkey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SharingKey",
+                table: "Groups",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SharingKey",
+                table: "Groups");
+        }
+    }
+}
