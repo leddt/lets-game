bafb9a5937db47acf8a03d8c478920a841ffadda
David Thibault <david.thibault@sigmund.ca>
2025-03-03 16:47:22 -0500

    Add endpoint to run tasks

---
+++ LetsGame.Web/Controllers/RecurringTasksController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using LetsGame.Web.RecurringTasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsGame.Web.Controllers;
+
+public class RecurringTasksController : Controller
+{
+    private readonly RecurringTaskRunner _runner;
+
+    public RecurringTasksController(RecurringTaskRunner runner)
+    {
+        _runner = runner;
+    }
+
+    [HttpPost("run-recurring-tasks")]
+    public async Task<IActionResult> RunTasks()
+    {
+        await _runner.RunAll();
+        return NoContent();
+    }
+}
