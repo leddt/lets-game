93b914c370c4bbd5b5883bea58ddc208550392b7
David Thibault <david.thibault@sigmund.ca>
2025-03-20 19:08:30 -0400

    Add transition to group-sidebar

--- LetsGame.Frontend/src/components/group/group-sidebar.svelte
+++ LetsGame.Frontend/src/components/group/group-sidebar.svelte
@@ -30,6 +30,8 @@
   import { useNavigate } from "svelte-navigator";
   import Button from "@/components/ui/button.svelte";
   import Section from "@/components/ui/section.svelte";
+  import { scale } from "svelte/transition";
+  import { writable } from "svelte/store";
 
   import SidebarMembers from "./sidebar-members.svelte";
   import SidebarAvailability from "./sidebar-availability.svelte";
@@ -40,6 +42,21 @@
   export let group;
 
   const navigate = useNavigate();
+
+  let nextIndex = 0;
+  const baseDelay = 75;
+
+  // Create a function to get transition params
+  const getTransition = () => ({
+    duration: 200,
+    delay: baseDelay * nextIndex++,
+    start: 0.95,
+  });
+
+  // Reset counter when group changes
+  $: if (group) {
+    nextIndex = 0;
+  }
 
   $: isOwner = group?.self.role === "OWNER";
 
@@ -65,41 +82,56 @@
 </script>
 
 <div>
-  <div class="sm:w-72 pb-4 sm:pt-4 px-4 sm:pl-0 flex flex-col gap-4">
-    <Section title="Members"><SidebarMembers {group} /></Section>
-    {#if group?.members.length > 1}
-      <Section title="Available?"><SidebarAvailability {group} /></Section>
-    {/if}
-    {#if group?.invites}
-      <Section title="Invites">
-        <div slot="right">
-          {#if isOwner}
-            <Button
-              tip="Create new single-use invite"
-              on:click={() => createInvite(true)}
-            >
-              + &#9312;
-            </Button>
-            <Button
-              tip="Create new unlimited invite"
-              on:click={() => createInvite(false)}
-            >
-              + &infin;
-            </Button>
-          {/if}
-        </div>
-        <SidebarInvites {group} />
-      </Section>
-    {/if}
-    <Section title="Games">
-      <div slot="right">
-        {#if isOwner}
-          <Button slot="right" on:click={() => navigate("add-game")}>
-            Add
-          </Button>
-        {/if}
+  {#key group?.id}
+    <div class="sm:w-72 pb-4 sm:pt-4 px-4 sm:pl-0 flex flex-col gap-4">
+      <div in:scale={getTransition()}>
+        <Section title="Members">
+          <SidebarMembers {group} />
+        </Section>
       </div>
-      <SidebarGames {group} />
-    </Section>
-  </div>
+
+      {#if group?.members.length > 1}
+        <div in:scale={getTransition()}>
+          <Section title="Available?">
+            <SidebarAvailability {group} />
+          </Section>
+        </div>
+      {/if}
+
+      {#if group?.invites}
+        <div in:scale={getTransition()}>
+          <Section title="Invites">
+            <div slot="right">
+              {#if isOwner}
+                <Button
+                  tip="Create new single-use invite"
+                  on:click={() => createInvite(true)}
+                >
+                  + &#9312;
+                </Button>
+                <Button
+                  tip="Create new unlimited invite"
+                  on:click={() => createInvite(false)}
+                >
+                  + &infin;
+                </Button>
+              {/if}
+            </div>
+            <SidebarInvites {group} />
+          </Section>
+        </div>
+      {/if}
+
+      <div in:scale={getTransition()}>
+        <Section title="Games">
+          <div slot="right">
+            {#if isOwner}
+              <Button on:click={() => navigate("add-game")}>Add</Button>
+            {/if}
+          </div>
+          <SidebarGames {group} />
+        </Section>
+      </div>
+    </div>
+  {/key}
 </div>
